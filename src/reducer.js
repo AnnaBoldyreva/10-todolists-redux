13d3d4cd1a86025019f0226689fa19515761ef1b
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-12-06 15:02:21 +0300

    Initial commit

---
+++ src/reducer.js
@@ -0,0 +1,81 @@
+const initialState = {
+    "todolists": [
+        {
+            "id": 0, "title": "every day",
+            tasks: [
+                {"title": "css11", "isDone": false, "priority": "low", "id": 0},
+                {"title": "js", "isDone": false, "priority": "low", "id": 1},
+                {"title": "react", "isDone": false, "priority": "low", "id": 2},
+                {"title": "sasasa", "isDone": false, "priority": "low", "id": 3},
+                {"title": "yoaa", "isDone": false, "priority": "low", "id": 4},
+                {"title": "sddsdsds", "isDone": false, "priority": "low", "id": 5}]
+        },
+        {"id": 1, "title": "tomorrow", tasks: []},
+        {"id": 2, "title": "weewwe`", tasks: []},
+        {"id": 3, "title": "dddd", tasks: []}
+    ]
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "ADD-TODOLIST":
+            return {
+                ...state,
+                todolists: [...state.todolists, action.newTodolist]
+            }
+        case "DELETE-TODOLIST":
+            return {
+                ...state,
+                todolists: state.todolists.filter(tl => tl.id != action.todolistId)
+            }
+        case "DELETE-TASK":
+            return {
+                ...state,
+                todolists: state.todolists.map(tl => {
+                    if (tl.id === action.todolistId) {
+                        return {
+                            ...tl,
+                            tasks: tl.tasks.filter(t => t.id != action.taskId)
+                        }
+                    } else {
+                        return tl
+                    }
+                })
+            }
+        case "ADD-TASK":
+            return {
+                ...state,
+                todolists: state.todolists.map(tl => {
+                    if (tl.id === action.todolistId) {
+                        return {...tl, tasks: [...tl.tasks, action.newTask]}
+                    } else {
+                        return tl
+                    }
+                })
+            }
+        case "UPDATE-TASK":
+            return {
+                ...state,
+                todolists: state.todolists.map(tl => {
+                    if (tl.id === action.todolistId) {
+                        return {
+                            ...tl,
+                            tasks: tl.tasks.map(t => {
+                                if (t.id != action.taskId) {
+                                    return t;
+                                } else {
+                                    return {...t, ...action.obj};
+                                }
+                            })
+                        }
+                    } else {
+                        return tl
+                    }
+                })
+            }
+    }
+    console.log("reducer: ", action);
+    return state;
+};
+
+export default reducer;
